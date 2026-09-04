d29cacd56bc103462ceaaa4b807a357182660506
Farbod <Farboddaneshmandfard@gmail.com>
2026-09-05 01:40:50 +0330

    robots.txt added

---
+++ robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next'
+import { siteConfig } from '@/lib/seo/config'
+
+const robots = (): MetadataRoute.Robots => {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/admin',
+    },
+    sitemap: `${siteConfig.url}/sitemap.xml`,
+  }
+}
+
+export default robots
